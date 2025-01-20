8c6a07b82d547494a4861252429039f45f4f8441
Sammïa Dergent <136450680+sammiadergent@users.noreply.github.com>
2025-01-20 20:23:51 +0100

    styling

--- src/main.js
+++ src/main.js
@@ -1,2 +1,21 @@
-import './style.css'
+import './style.css';
+import Swiper from 'swiper/bundle';
+import 'swiper/swiper-bundle.css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
+
+
+const swiper = new Swiper('.swiper', {
+  loop: true,
+  slidesPerView: 1, // Toon één slide per keer
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+});
 
